7f870cc9587d8e39adb4ba78b83be1b6bcd3531d
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2022-04-07 18:32:10 +0530

    bug fix

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
     res.send(html);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));

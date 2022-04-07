ab65c91bcc9682dd2baddb3815e014d907fb1ded
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2022-04-07 18:27:07 +0530

    app finish

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const html = `
+<h1>Abhishek Agarwal</h1>
+<h2>Reg. No. - 20BDS0070</h2>
+<p>Lab Cat 07/04/2022</p>
+`;
+
+app.get('/', (req, res) => {
+    res.send(html);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
